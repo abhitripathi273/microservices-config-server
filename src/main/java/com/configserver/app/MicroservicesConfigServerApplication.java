25194018337530adcd7c960452fb7afc2fe062ed
abhtripa <abhishek.tripathi@publicissapient.com>
2020-04-30 22:22:17 +0530

    Added microservices config server files

---
+++ src/main/java/com/configserver/app/MicroservicesConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.configserver.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesConfigServerApplication.class, args);
+	}
+
+}
